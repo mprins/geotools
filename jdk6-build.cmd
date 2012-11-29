2ef1cae465d9809c58d196975aaab812a792dbc4
Mark Prins <mprins@users.sf.net>
2012-11-29 13:37:19 +0100

    local build files

---
+++ jdk6-build.cmd
@@ -0,0 +1,2 @@
+set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_37
+mvn -fae -Dall -T2.2C -U clean install
---
+++ jdk7-build.cmd
@@ -0,0 +1,2 @@
+set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_09
+mvn -fae -Dall -T2.2C -U clean install
